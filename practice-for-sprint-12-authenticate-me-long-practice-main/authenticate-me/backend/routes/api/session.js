a16fc281194a98b8292249cc886416a9a8fe85bd
Gabriel <gabetd0904@gmail.com>
2022-10-13 11:39:41 -0500

    Add User logout backend endpoint

--- practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/session.js
+++ practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/session.js
@@ -34,7 +34,19 @@ router.post(
   }
 );
 
+// backend/routes/api/session.js
+// ...
 
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+// ...
 
 
 // backend/routes/api/session.js
