658783f2718e962b21ec2d1491ada777f59a19e9
Gabriel <gabetd0904@gmail.com>
2022-10-13 12:14:14 -0500

    fixed accidental deletion of delete route in backend > routes > api > session.js

--- practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/session.js
+++ practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/session.js
@@ -58,6 +58,39 @@ router.post(
   }
 );
 
+// backend/routes/api/session.js
+// ...
+
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+// ...
+
+
+// backend/routes/api/session.js
+// ...
+
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
+// ...
 
 
 
